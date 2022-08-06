40d0e10e50aa8a54a49ab1d17b6cb5668b6b601e
Sasa Savic Perusina <sasa@bubulazy.com>
2022-08-06 13:14:41 +0100

    Proyecto terminado
    En la realidad falta muchas cosas en CSS pero no hay tiempo y tampoco era subjeto del proyecto

--- mi-lista.js
+++ mi-lista.js
@@ -1,7 +1,17 @@
 const watchlistContainer = document.querySelector('#watchlist-container')
 let watchList = []
 
+// Aqui cargamos watchlist desde local storage (si hay)
+function loadWatchlistFromStorage() {
+  const fromLocal = window.localStorage.getItem('myWatchlist')
+  watchList = JSON.parse(fromLocal)
+  // Llamamos la funcion que genera HTML pasando watchlist como argumento
+  getHTML(watchList)
+}
+
+// Sirve para generar HTML con el contenido de watchlist
 function getHTML(watchList) {
+  // Hacemos un .map() sobre watchlist
   const html = watchList.map( movie => {
     return `
     <section class="card-watchlist-page">
@@ -24,30 +34,34 @@ function getHTML(watchList) {
   <hr>
     `
   } ).join('')
+  // Render HTML en el navegador
   watchlistContainer.innerHTML = html
+  // Despues de crear botones de remove from watchlist les añadimos eventListener
   removeBtnEventListeners()
 }
 
+// Añadir eventListener a botones de remove
 function removeBtnEventListeners() {
+  // Creamos una HTML coclection
   const btnColection = document.querySelectorAll('#card-remove-btn')
+  // La expandimos en un array con spread operator
   const btnArray = [...btnColection]
+
   btnArray.map( btn => {
     btn.addEventListener('click', () => {
-
+      // Usamos el atributo data- de HTML para usar su valor para eliminar la peli desde watchlist y local storage
       const btnData = btn.getAttribute('data-movie-title')
+      // Encontramos indice de nuestra peli
       const indice = watchList.findIndex( movie => movie.Title === btnData)
+      // Eliminamos la peli usando aplice()
       watchList.splice(indice, 1)
+      // Guardamos la nueva watchlist de nuevo en local storage
       window.localStorage.setItem('myWatchlist', JSON.stringify(watchList))
+      // Cargamos la watchlist desde local storage de nuevo.
       loadWatchlistFromStorage()
 
     }) 
   } )
-}
-
-function loadWatchlistFromStorage() {
-  const fromLocal = window.localStorage.getItem('myWatchlist')
-  watchList = JSON.parse(fromLocal)
-  getHTML(watchList)
 }
 
 loadWatchlistFromStorage()
